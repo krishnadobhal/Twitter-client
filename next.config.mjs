a01e0f1d1ebdd90db0ce28930ac5f14946085584
krishnadobhal <krishnadobhal234@gmail.com>
2024-11-23 23:39:22 +0530

    first

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['avatars.githubusercontent.com','lh3.googleusercontent.com','twitter-krishna.s3.ap-south-1.amazonaws.com'],
+    },
+};
+
+export default nextConfig;
