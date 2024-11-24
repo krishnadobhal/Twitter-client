2a645b654808a804625f105d16c49298f4a7cb11
krishnadobhal <krishnadobhal234@gmail.com>
2024-11-24 12:36:16 +0530

    server updaated

--- codegen.ts
+++ codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: process.env.NEXT_PUBLIC_API_URL,
+  schema: "http://52.66.250.11:8000/graphql",
   documents: "**/*.{tsx,ts}",
   generates: {
     "gql/": {
